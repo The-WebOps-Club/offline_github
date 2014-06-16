d45acbc8a97116fc2456b9bcaf34d0809a9c551c
sahitilucky <sahitilucky21@gmail.com>
2014-06-16 15:54:44 +0530

    updated views.py with os module

--- project1/offline_github/views.py
+++ project1/offline_github/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
+import os
 from django.http import HttpResponse
 from django.template import Template,Context
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
-
+PROJECT_PATH=os.path.abspath(os.path.dirname('project1'))
 def home(request):
 	return render(request,'home.html')
 
@@ -15,9 +16,11 @@ def offgit_sign(request):
 	return render(request,'sign.html')
 
 def user(request):
-	fp=open("/home/keerthana/project1/templates/trial.html")
+	a=(os.path.join(PROJECT_PATH,'templates','trial.html'))
+	fp=open(a)
 	t=Template(fp.read())
-	q=open("/home/keerthana/Downloads/access_token","rb").read()
+	c=os.path.expanduser("~/Downloads/access_token")
+	q=open(c,"rb").read()
 	w=q.split("=")[1]
 	e=w.split("&")[0]
         html=t.render(Context({'token':e,'oauth':q})) 
