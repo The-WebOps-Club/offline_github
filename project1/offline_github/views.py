e908766cd2d4293c7eeea030ce123f8a837d4bc1
sahitilucky <sahitilucky@users.noreply.github.com>
2014-07-04 13:51:53 +0530

    edited views.py

--- project1/offline_github/views.py
+++ project1/offline_github/views.py
@@ -6,6 +6,23 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 PROJECT_PATH=os.path.abspath(os.path.dirname('project1'))
+def get_file():
+	s=os.path.expanduser("~/Downloads")
+	c=os.listdir(s)
+	a=[]
+	for x in range(len(c)):
+		if 'access_token' in c[x]:
+			if os.path.getsize(s+"/"+c[x])==96 :
+				a=a+[c[x]]
+	b=[]
+	if len(a)==1:
+		return a[0]
+	else:
+		for x in range(len(a)):
+			if len(a[x].split("("))>1:
+				b=b+[int(a[x].split("(")[1].split(")")[0])]
+	
+	return 'access_token'+'('+str(max(b))+')'
 def home(request):
 	return render(request,'home.html')
 
@@ -19,7 +36,8 @@ def user(request):
 	a=(os.path.join(PROJECT_PATH,'templates','trial.html'))
 	fp=open(a)
 	t=Template(fp.read())
-	c=os.path.expanduser("~/Downloads/access_token")
+	c=get_file()
+	c=os.path.expanduser("~/Downloads/"+c)
 	q=open(c,"rb").read()
 	w=q.split("=")[1]
 	e=w.split("&")[0]
